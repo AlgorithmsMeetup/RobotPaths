71864667dcdb3a10c46cf0693e217f6034cc7dda
Drew Cuthbertson <drew@livelovely.com>
2015-08-22 12:19:03 -0700

    Initial commit with README and tests

---
+++ README.md
@@ -0,0 +1,4 @@
+###Robot Paths
+Imagine an n by m grid with a robot in the upper left square.  If the robot can only move right or down, how many different paths are there for the robot to get to the bottom right corner?
+
+What if there are obstacles in the way?  What if the robot could also move up or left, but not visit the same space twice?  Can you make it work for very large grids?
---
+++ robotPaths.js
@@ -0,0 +1,7 @@
+var countPaths = function(grid){
+  /*
+   *  With the grid as the input, count the total number of paths from the upper left to bottom right corner.
+   *  Extra credit:  can you generalize this to work even if there are obstacles in the way? (obstacles will be represented by 0's)
+   *  Extra extra credit:  can you make your solution work for extremely large grids  (say, greater than 100x100?)
+   */
+}
